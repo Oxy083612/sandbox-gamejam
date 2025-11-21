f491f77326127721ac42b568c20e308cdc64d3fd
Oxy083612 <149008316+Oxy083612@users.noreply.github.com>
2025-11-21 19:12:19 +0100

    player movement

---
+++ scenes/player/girl2.tres.gd
@@ -0,0 +1,64 @@
+extends CharacterBody2D
+
+const SPEED = 150.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var table: StaticBody2D = $"../Table"
+@onready var area_2d: Area2D = $Area2D
+@onready var label_desc: Label = $"../HUDSearchItems/Label"
+
+
+enum DIRECTION {front, right, back, left}
+var dir = DIRECTION.front
+@export var item_held = null
+var can_get_items = false
+
+func _physics_process(_delta: float) -> void:	
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if(input_direction[0] > 0 and velocity != Vector2.ZERO):
+		sprite.play("walk_right")
+		dir = DIRECTION.right
+	if(input_direction[0] < 0 and velocity != Vector2.ZERO):
+		sprite.play("walk_left")
+		dir = DIRECTION.left
+	if(input_direction[1] > 0 and velocity != Vector2.ZERO):
+		sprite.play("walk_up")
+		dir = DIRECTION.front
+	if(input_direction[1] < 0 and velocity != Vector2.ZERO):
+		sprite.play("walk_down")
+		dir = DIRECTION.back
+	if velocity == Vector2.ZERO:
+		match dir:
+			DIRECTION.right:
+				sprite.play("idle_right")
+			DIRECTION.left:
+				sprite.play("idle_left")
+			DIRECTION.front:
+				sprite.play("idle_up")
+			DIRECTION.back:
+				sprite.play("idle_down")
+	velocity = input_direction * SPEED
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body) -> void:
+	
+	emit_signal("show_item", body.get_instance_id())
+	if not can_get_items:
+		return
+	if item_held == null and body != table:
+		item_held = body.item_name
+		label_desc.text = "Return the item to the table"
+		emit_signal("destroy_item", body.get_instance_id())
+	elif item_held != null and body == table:
+		label_desc.text = ""
+		item_held = null
+		
+		
+func _on_area_2d_body_exited(body) -> void:
+	emit_signal("hide_item", body.get_instance_id())
+
+func _input(event):
+	if event.is_action_pressed("pick_up"):
+		can_get_items = true
+	elif event.is_action_released("pick_up"):
+		can_get_items = false
