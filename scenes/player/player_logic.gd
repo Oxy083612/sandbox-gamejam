6c6f6ec5b01c226887499008bf888120882f5934
Wiktoria Zelawska <wikzel13@gmail.com>
2025-11-22 11:23:56 +0100

    Naprawa

--- scenes/player/player_logic.gd
+++ scenes/player/player_logic.gd
@@ -142,7 +142,3 @@ func decrease_health(healthAmount: int) -> bool:
 	print("Current player health: ", player_current_health)
 	print("Is player alive: ", is_player_alive)
 	return is_player_alive
-func player_sell_method():
-	pass
-func player_shop_method():
-	pass
--- scenes/shop.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody2D
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_method("player_shop_method"):
-		print("Open_Shop")
