faf06ee2664e4df9717445070fd758313d44a47e
Yuliia Tryshyna <juliatrishina93@gmail.com>
2025-11-22 09:45:49 +0100

    dzien-noc-menu, naprawione

--- _autoload/GlobalTime.gd
+++ _autoload/GlobalTime.gd
@@ -1,7 +1,5 @@
 extends Node
 
-# 1. DEFINE SIGNAL (The Notification)
-# This effectively replaces the "observers" list.
 signal day_night_changed()
 
 @onready var night_tint: ColorRect = $CanvasLayer/ColorRect
@@ -10,23 +8,29 @@ signal day_night_changed()
 
 func _ready():
 	night_tint.visible = false
-	timer.autostart
+	timer.autostart = true
 
-func _toggle_day_night():
-	is_day = not is_day
-	
-	day_night_changed.emit()
-	
-	
 	if is_day:
 		night_tint.visible = false
 		print(">>> DZIEN <<<")
-		timer.start(420)
+		timer.start(10)
 	else:
 		night_tint.visible = true
 		print(">>> NOC <<<")
 		timer.start(60)
 
+func _toggle_day_night():
+	is_day = not is_day
+	day_night_changed.emit()
+
+	if is_day:
+		night_tint.visible = false
+		print(">>> DZIEN <<<")
+		timer.start(10)
+	else:
+		night_tint.visible = true
+		print(">>> NOC <<<")
+		timer.start(60)
 
 func _on_timer_timeout() -> void:
 	_toggle_day_night()
