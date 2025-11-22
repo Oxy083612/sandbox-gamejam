2a0595e42d6bfcc171ea01cda91984d86c20079a
Oxy083612 <149008316+Oxy083612@users.noreply.github.com>
2025-11-22 13:00:41 +0100

    combat-mechanic

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -2,12 +2,18 @@ extends CharacterBody2D
 
 @export var target: CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var damage_intake: Timer = $damage_intake
 
 var speed = 80
+var health = 25
+var player_in_attack_zone = false
+var can_take_damage = true
 
 func _physics_process(delta: float) -> void:
 	if target == null:
 		return
+
+	deal_with_damage()
 
 	var distance = global_position.distance_to(target.global_position)
 	
@@ -24,3 +30,30 @@ func _physics_process(delta: float) -> void:
 			
 
 	move_and_slide()
+
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_attack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_attack_zone = false
+		
+func deal_with_damage():
+	if player_in_attack_zone and Combat.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			damage_intake.start()
+			can_take_damage = false
+			print("slime health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_damage_intake_timeout() -> void:
+	can_take_damage = true
--- scenes/player/player_logic.gd
+++ scenes/player/player_logic.gd
@@ -2,15 +2,23 @@ extends CharacterBody2D
 
 class_name Player
 
-
 const SPEED = 150.0
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var area_2d: Area2D = $Area2D
+@onready var damage_intake: Timer = $damage_intake
+@onready var attack_cooldown: Timer = $attack_cooldown
 
 @onready var inventoryUI: Control = $Inventory_UI
 @export var inventory: Inventory
 
 var nearby_plant: Plant = null
+
+var enemy_in_attack_range = false
+var damage_intake_cooldown = true
+var player_alive = true
+
+var attack_in_progress = false
+
 
 enum DIRECTION {front, right, back, left}
 var dir = DIRECTION.front
@@ -22,30 +30,33 @@ func _physics_process(_delta: float) -> void:
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if inventoryUI.is_open:
 		return
-	if(input_direction[0] > 0 and velocity != Vector2.ZERO):
-		sprite.play("walk_right")
-		dir = DIRECTION.right
-	if(input_direction[0] < 0 and velocity != Vector2.ZERO):
-		sprite.play("walk_left")
-		dir = DIRECTION.left
-	if(input_direction[1] > 0 and velocity != Vector2.ZERO):
-		sprite.play("walk_down")
-		dir = DIRECTION.front
-	if(input_direction[1] < 0 and velocity != Vector2.ZERO):
-		sprite.play("walk_up")
-		dir = DIRECTION.back
-	if velocity == Vector2.ZERO:
-		match dir:
-			DIRECTION.right:
-				sprite.play("idle_right")
-			DIRECTION.left:
-				sprite.play("idle_left")
-			DIRECTION.front:
-				sprite.play("idle_down")
-			DIRECTION.back:
-				sprite.play("idle_up")
-	velocity = input_direction * SPEED
-	move_and_slide()
+	attack()
+	if !attack_in_progress:
+		if(input_direction[0] > 0 and velocity != Vector2.ZERO):
+			sprite.play("walk_right")
+			dir = DIRECTION.right
+		if(input_direction[0] < 0 and velocity != Vector2.ZERO):
+			sprite.play("walk_left")
+			dir = DIRECTION.left
+		if(input_direction[1] > 0 and velocity != Vector2.ZERO):
+			sprite.play("walk_down")
+			dir = DIRECTION.front
+		if(input_direction[1] < 0 and velocity != Vector2.ZERO):
+			sprite.play("walk_up")
+			dir = DIRECTION.back
+		if velocity == Vector2.ZERO:
+			match dir:
+				DIRECTION.right:
+						sprite.play("idle_right")
+				DIRECTION.left:
+						sprite.play("idle_left")
+				DIRECTION.front:
+						sprite.play("idle_down")
+				DIRECTION.back:
+						sprite.play("idle_up")
+		velocity = input_direction * SPEED
+		move_and_slide()
+	enemy_attack()
 
 
 func _on_area_2d_body_entered(_body) -> void:
@@ -132,13 +143,62 @@ func add_health(health_amount: int) -> void:
 	print("Current player health: ", player_current_health)
 		
 #	Function returns state of player using bool value
-func decrease_health(healthAmount: int) -> bool:
-	var is_player_alive: bool = true
+func decrease_health(healthAmount: int):
 	if player_current_health - healthAmount <= min_player_health:
 		player_current_health = min_player_health
-		is_player_alive = false
+		player_alive = false
 	else:
 		player_current_health -= healthAmount
-	print("Current player health: ", player_current_health)
-	print("Is player alive: ", is_player_alive)
-	return is_player_alive
+	
+
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_attack_range = true
+
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_attack_range = false
+
+func player():
+	pass
+
+func enemy_attack():
+	if enemy_in_attack_range and damage_intake_cooldown == true:
+		decrease_health(20)
+		damage_intake_cooldown = false
+		damage_intake.start()
+		print(player_current_health)
+		if !player_alive:
+
+			self.queue_free()
+		
+
+func attack():
+	if Input.is_action_just_pressed("attack"):
+		Combat.player_current_attack  = true
+		attack_in_progress = true
+		attack_cooldown.start()
+		if dir == DIRECTION.right:
+			sprite.flip_h = false
+			sprite.play("side_attack")
+		if dir == DIRECTION.left:
+			sprite.flip_h = true
+			sprite.play("side_attack")
+		if dir == DIRECTION.front:
+			sprite.play("up_attack")
+		if dir == DIRECTION.back:
+			sprite.play("back_attack")
+			
+			
+
+func _on_damage_intake_timeout() -> void:
+	damage_intake_cooldown = true # Replace with function body.
+
+
+func _on_attack_cooldown_timeout() -> void:
+	attack_cooldown.stop()
+	Combat.player_current_attack = false
+	attack_in_progress = false
+	
